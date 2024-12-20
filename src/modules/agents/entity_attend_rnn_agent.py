5ee4675ebab6e2f8c60a56fb1205c210896e0ee2
loren <loren5555@sina.com>
2024-12-20 22:14:07 +0800

    Optimize thr structure of entity attention agent.

--- src/modules/agents/entity_attend_rnn_agent.py
+++ src/modules/agents/entity_attend_rnn_agent.py
@@ -3,10 +3,9 @@ from types import SimpleNamespace
 import torch.nn
 import torch.nn as nn
 
+from .agent import Agent
 from modules.layers import EntityAttnLayer
 from utils.rms_norm import RMSNorm
-from utils.custom_logging import PyMARLLogger
-from .agent import Agent
 
 
 class EntityAttnRNNAgent(Agent):
@@ -15,47 +14,40 @@ class EntityAttnRNNAgent(Agent):
 
         self.args = args
         self.device = args.device
-        self.n_agents: int = getattr(args, "n_agents")
         self.n_heads: int = getattr(args, "agent_attn_heads")   # Attention heads.
         self.hidden_dim: int = getattr(args, "agent_hidden_dim")    # Dimension of embedding andRNN.
         self.attn_dim: int = getattr(args, "agent_attn_dim")    # Dimension of full attention layer
         self.gru_layers: int = getattr(args, "agent_gru_layers")    # Number of GRU layers.
 
-        # Ensure attention dimension is divisible by the number of heads
-        if self.attn_dim % self.n_heads != 0:
-            PyMARLLogger("main").get_child_logger(f"{self.__class__.__name__}").Fatal(
-                f"Attention dimension must be divisible by number of heads. Current values: dim: {self.attn_dim}, heads: {self.n_heads}"
-            )
-            raise ValueError("Attention dimension must be divisible by number of heads.")
-
         # Embedding layers: scheme -> hidden_dim
         self.embedding_layers = nn.ModuleList()
         for feat_name, feat_shape in input_scheme[0].items():
             self.embedding_layers.append(
-                nn.Linear(feat_shape[1], self.hidden_dim, bias=False, )
+                nn.Linear(feat_shape[1], self.hidden_dim, bias=False, device=self.device)
             )
 
         # Embedding layers: 1 -> hidden_dim
         for feat_name, feat_shape in input_scheme[1].items():
             self.embedding_layers.append(
-                nn.Embedding(feat_shape[1], self.hidden_dim, )
+                nn.Embedding(feat_shape[1], self.hidden_dim, device=self.device)
             )
 
         # Encoding layers: hidden_dim -> attn_dim
         self.encoding = nn.Sequential(
-            nn.Linear(self.hidden_dim, self.attn_dim, ),
+            nn.Linear(self.hidden_dim, self.attn_dim, device=self.device),
             nn.LeakyReLU(inplace=True),
         )
 
         self.attn = EntityAttnLayer(args, self.attn_dim, self.n_heads)
-        self.feedforward = nn.Linear(self.attn_dim, self.attn_dim, bias=False)
-        self.norm = RMSNorm(self.attn_dim, eps=1e-5, elementwise_affine=True)
+        self.norm1 = RMSNorm(self.attn_dim, eps=1e-5, elementwise_affine=True).to(self.device)
+        self.feedforward = nn.Linear(self.attn_dim, self.attn_dim, bias=False, device=self.device)
+        self.norm2 = RMSNorm(self.attn_dim, eps=1e-5, elementwise_affine=True).to(self.device)
 
         # Output layers: attn_dim -> hidden_dim -> n_actions q
-        self.rnn_proj = nn.Linear(self.attn_dim, self.hidden_dim, )
+        self.rnn_proj = nn.Linear(self.attn_dim, self.hidden_dim, device=self.device)
         # TODO: RNN might not be advanced. Transformer decoder seems to work here.
-        self.rnn = nn.GRU(self.hidden_dim, self.hidden_dim, num_layers=self.gru_layers, batch_first=True)
-        self.decoding = nn.Linear(self.hidden_dim, args.n_actions, )
+        self.rnn = nn.GRU(self.hidden_dim, self.hidden_dim, num_layers=self.gru_layers, batch_first=True, device=self.device)
+        self.decoding = nn.Linear(self.hidden_dim, args.n_actions, device=self.device)
 
     def init_hidden(self):
         # make hidden states on same device as model
@@ -63,31 +55,31 @@ class EntityAttnRNNAgent(Agent):
         # return self.own_embed.weight.new(1, self.args.agent_hidden_dim).zero_()
 
     def forward(self, inputs, hidden_state):
-        # Head
-        x = [
-            layer(feature_input) for feature_input, layer in zip(inputs, self.embedding_layers)
-        ]
-
-        x = torch.cat(x, dim=-2)
+        # Embedding: scheme -> hidden_dim
+        entities = torch.cat(
+            [layer(feature_input) for feature_input, layer in zip(inputs, self.embedding_layers)],
+            dim=-2
+        )
 
         # Encoding: hidden_dim -> attn_dim
         # A single transformer encoder.
         # TODO: Test multiple structure of attention.
-        x = self.encoding(x)  # TODO: Maybe not useful because all information has already been embedded.
-        x = self.norm(x[..., 0, :] + self.attn(x))
-        x = self.norm(x + self.feedforward(x))
-
+        entities = self.encoding(entities)
+        attn = self.norm1(entities[..., 0, :] + self.attn(entities))
+        attn = self.norm2(attn + self.feedforward(attn))
 
         # TODO: After the first entity attention layer, the rest should be self attention layer. Not implemented.
 
+        batch_size, time_size, n_agents, _ = attn.shape
+
         # Output:  attn_dim -> n_actions
-        batch_size, time_size, n_agents, _ = x.shape
-        x = self.rnn_proj(x)     # attn_dim -> hidden_dim
+        x = self.rnn_proj(attn)     # attn_dim -> hidden_dim
 
         x = x.transpose(1, 2).reshape(batch_size * n_agents, time_size, self.hidden_dim)  # b * t * n * d -> b * n * t * d -> (b * n) * t * d
         h = hidden_state.reshape(self.gru_layers, batch_size * n_agents, self.hidden_dim)
 
-        x, h = self.rnn(x, h)
+        x, h = self.rnn(x, h)   # GRU forward.
+
         x = x.reshape(batch_size, n_agents, time_size, self.hidden_dim).transpose(1, 2)
         h = h.reshape(self.gru_layers, batch_size, n_agents, self.hidden_dim)
 
--- src/modules/layers/entity_attention.py
+++ src/modules/layers/entity_attention.py
@@ -4,14 +4,14 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as functional
 
-from utils.rms_norm import RMSNorm
+from utils.custom_logging import PyMARLLogger
 
 
 class EntityAttnLayer(nn.Module):
     """
-    Attention Layer for entity scheme multi-agent systems..
+    Attention Layer for entity scheme multi-agent systems.
     Args:
         args: args initialized in pymarl.
         embed_dim: size of embedding feature.
@@ -21,7 +21,8 @@ class EntityAttnLayer(nn.Module):
             self,
             args: SimpleNamespace,
             embed_dim: int,
-            num_heads: int
+            num_heads: int,
+            device: Optional[torch.device] = None
     ):
         super(EntityAttnLayer, self).__init__()
         self.args = args
@@ -29,50 +30,46 @@ class EntityAttnLayer(nn.Module):
         # Attn arguments.
         self.embed_dim = embed_dim
         self.num_heads = num_heads
+
+        # Ensure attention dimension is divisible by the number of heads
+        if self.embed_dim % self.num_heads != 0:
+            PyMARLLogger("main").get_child_logger(f"{self.__class__.__name__}").Fatal(
+                f"Attention dimension must be divisible by number of heads. Current values: dim: {self.embed_dim}, heads: {self.num_heads}"
+            )
+            raise ValueError("Attention dimension must be divisible by number of heads.")
+
         self.head_dim = embed_dim // self.num_heads
         self.scaling = self.head_dim ** -0.5
 
         # networks
-        self.q_proj = nn.Linear(embed_dim, embed_dim, bias=False)
-        self.k_proj = nn.Linear(embed_dim, embed_dim, bias=False)
-        self.v_proj = nn.Linear(embed_dim, embed_dim, bias=False)
-        self.out_proj = nn.Linear(embed_dim, embed_dim, bias=False)
+        self.q_proj = nn.Linear(embed_dim, embed_dim, bias=False, device=device)
+        self.k_proj = nn.Linear(embed_dim, embed_dim, bias=False, device=device)
+        self.v_proj = nn.Linear(embed_dim, embed_dim, bias=False, device=device)
+        self.out_proj = nn.Linear(embed_dim, embed_dim, bias=False, device=device)
 
     def extra_repr(self):
         return f"embed_dim={self.embed_dim}, heads={self.num_heads}"
 
     def forward(
             self,
-            entities: torch.Tensor,
-
-            agent_mask: torch.Tensor = None
-    ) -> (torch.Tensor, torch.Tensor):
+            entities: torch.Tensor
+    ) -> torch.Tensor:
         """
         Args:
-            entities: Entity representations.
+            entities (torch.Tensor): Entity representations.
                 [batch size, time size, n_agents, n_entities, embedding dimension]
 
-            agent_mask: Which agents/entities are not available. Zero out their outputs tow
-                prevent gradients from flowing back. Shape of 2nd dim determines
-                whether to compute queries for all entities or just agents.
-                batch size, # of agents or entities]
-
         Return:
-            tuple(attn, attn_weights):
-            - attn: attentioned entities state., [batch size, n_agents, embedding dimension]
-            - attn_weights: attention weights from agent to entities. [batch size, n_agents, embedding dimension]
+            attn (torch.Tensor): attend entities state., [batch size, n_agents, embedding dimension]
         """
-        batch_size, time_size, n_agents, n_entities, embed_dim = entities.shape
-
         # calculate q, k, v.
         q = self.q_proj(entities[..., 0, :].unsqueeze(-2))  # batch * time * agents * self(1) * embed_dim
         k = self.k_proj(entities)  # batch * time * agents * n_entities * embed_dim
         v = self.v_proj(entities)  # batch * time * agents * n_entities * embed_dim
 
-        q = q.reshape(*q.shape[:-1], self.num_heads, -1)  # batch * time * agents * self(1) * heads * head_dim
-        q = q * self.scaling
-        k = k.reshape(*k.shape[:-1], self.num_heads, -1)  # batch * time * agents * n_entities * heads * head_dim
-        v = v.reshape(*v.shape[:-1], self.num_heads, -1)  # batch * time * agents * n_entities * heads * head_dim
+        q = q.reshape(*q.shape[:-1], self.num_heads, self.head_dim)  # batch * time * agents * self(1) * heads * head_dim
+        k = k.reshape(*k.shape[:-1], self.num_heads, self.head_dim)  # batch * time * agents * n_entities * heads * head_dim
+        v = v.reshape(*v.shape[:-1], self.num_heads, self.head_dim)  # batch * time * agents * n_entities * heads * head_dim
 
         q = q.transpose(-2, -3)  # batch * time * agents * heads * self(1)  * head_dim
         k = k.transpose(-2, -3)  # batch * time * agents * heads * n_entities * head_dim
@@ -80,11 +77,8 @@ class EntityAttnLayer(nn.Module):
 
         # calculate attention weights.
         attn_weights = q @ k.transpose(-2, -1)  # batch * time * agents * heads * 1  * head_dim
-        attn_weights = F.softmax(
-            attn_weights,
-            dim=-1,
-            dtype=torch.float32
-        ).type_as(attn_weights)
+        attn_weights = attn_weights * self.scaling
+        attn_weights = functional.softmax(attn_weights, dim=-1)
 
         attn = attn_weights @ v     # batch * time * agents * heads * 1  * head_dim
         attn = attn.transpose(-2, -3).reshape(*attn.shape[:-3], self.num_heads * self.head_dim)
@@ -92,7 +86,3 @@ class EntityAttnLayer(nn.Module):
         attn = self.out_proj(attn)  # batch * agents * embedding_dim
 
         return attn
-
-    @staticmethod
-    def _lambda_inti_fn(depth):
-        return 0.8 - 0.6 * math.exp(-0.3 * depth)
